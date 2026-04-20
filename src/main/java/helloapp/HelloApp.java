2bec38180756f1e34ead432cd7aee37b319b24c1
kanakalarohit <rohitkanakala@gmail.com>
2026-04-20 22:43:36 +0530

    UC7: Add HelloApp program with greeting logic

--- src/main/java/helloapp/HelloApp.java
+++ src/main/java/helloapp/HelloApp.java
@@ -1,7 +1,16 @@
 package helloapp;
 
 public class HelloApp {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        String names;
+        if (args.length == 0) {
+            names = "World";
+        } else {
+            names = String.join(", ", args);
+        }
+
+        System.out.println("Hello, " + names + "!");
     }
 }
